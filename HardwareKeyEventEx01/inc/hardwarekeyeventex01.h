e91cdda4a7f9ef13a9eb3c5db28345469da55f24
codedragon <codedragon@tistory.com>
2016-03-20 16:24:06 +0900

    add

---
+++ HardwareKeyEventEx01/inc/hardwarekeyeventex01.h
@@ -0,0 +1,19 @@
+#ifndef __hardwarekeyeventex01_H__
+#define __hardwarekeyeventex01_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "hardwarekeyeventex01"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.hardwarekeyeventex01"
+#endif
+
+#endif /* __hardwarekeyeventex01_H__ */
